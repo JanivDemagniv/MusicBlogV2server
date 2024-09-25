74d55ddbfe0c210c64569da33665afd52a86d468
JanivDemagniv <janivjry@gmail.com>
2024-09-25 15:20:22 +0300

    watch one users and all users functions

--- users/routes/userRestController.js
+++ users/routes/userRestController.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { handleError } = require('../../utils/handleError');
-const { createUser, loginUser } = require('../models/userAccessDataService');
+const { createUser, loginUser, getUser, getAllUsers } = require('../models/userAccessDataService');
+const auth = require('../../auth/authServices');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
         let newUser = await createUser(req.body);
-        return newUser;
+        res.send(newUser);
     } catch (error) {
-        handleError(res, 400, error.message)
+        handleError(res, 400, error.message);
     }
 });
 
@@ -17,6 +18,38 @@ router.post('/login', async (req, res) => {
         let { email, password } = req.body;
         const token = await loginUser(email, password);
         res.send(token)
+    } catch (error) {
+        handleError(res, 400, error.message)
+    }
+});
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userInfo = req.user;
+        if (userInfo._id != id && !userInfo.isAdmin) {
+            let error = new Error;
+            error.message = 'You are not authorize to see that user details';
+            handleError(res, 403, error);
+        }
+        const user = await getUser(id);
+        res.send(user)
+    } catch (error) {
+        handleError(res, 400, error.message)
+    }
+});
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const userInfo = req.user;
+        if (!userInfo.isAdmin) {
+            let error = new Error;
+            error.message = "You are not authorize to see all users";
+            handleError(res, 403, error);
+        };
+
+        const allUsers = await getAllUsers();
+        res.send(allUsers)
     } catch (error) {
         handleError(res, 400, error.message)
     }
